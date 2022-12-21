f00d36b10c547b9a482152c72133d26da3988049
sahilsalam25 <sahilsalamece25@gmail.com>
2022-12-21 10:29:06 +0530

    name changed

--- MonyMonyBankApplication/src/com/tns/client/MMClient.java
+++ MonyMonyBankApplication/src/com/tns/client/MMClient.java
@@ -8,9 +8,9 @@ public class MMClient {
 
 	public static void main(String[] args) {
 		MMBankFactory factory=new MMBankFactory();
-		MMCurrentAcc gsn=(MMCurrentAcc)factory.getNewCurrentAccount(895581212, "Zakaur", 6040.00f);
+		MMCurrentAcc gsn=(MMCurrentAcc)factory.getNewCurrentAccount(895581212, "Ahmad", 6040.00f);
 		gsn.withdraw(gsn.getAccBal());
-		MMSavingAcc gsp=(MMSavingAcc) factory.getNewSavingAccount(1218529622, "Rahman", 5000.00f, true);
+		MMSavingAcc gsp=(MMSavingAcc) factory.getNewSavingAccount(1218529622, "Khan", 5000.00f, true);
 		gsp.withdraw(gsp.getAccBal());
 	}
 
